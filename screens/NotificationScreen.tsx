60e796a1f6508cc486c44df33b38382e82532b9d
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-05-20 14:12:42 +0200

    feat: install notifee

--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -1,18 +1,54 @@
 import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
 import { Props, RootStackParamList } from "../components/Props";
+import notifee from '@notifee/react-native';
 
-function sendNotification() {
-    //
-    //
+async function sendNotification() {
+  console.log("Hello World");
+
+  // Create a channel
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Default Channel',
+  });
+
+  // Display a notification
+  await notifee.displayNotification({
+    title: 'Notification Title',
+    body: 'Main body content of the notification',
+    android: {
+      channelId,
+      smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+    },
+  });
 }
 
 function NotificationScreen({ navigation }: Props) {
+  async function onDisplayNotification() {
+    // Create a channel
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+      },
+    });
+  }
+
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Notification Test Screen</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Send Notification" onPress={sendNotification} />
+      <Button title="Send Notification" onPress={() => sendNotification()} />
+      <Button title="Display Notification" onPress={() => onDisplayNotification()} />
     </View>
   );
 }
